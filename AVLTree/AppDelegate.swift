6a2a2ae507eb68b7ee40648bce2aa91f08309ea3
Alex Nichol <unixpickle@gmail.com>
2014-06-10 18:38:01 -0400

    Initial Commit

---
+++ AVLTree/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  AVLTree
+//
+//  Created by Alex Nichol on 6/10/14.
+//  Copyright (c) 2014 Alex Nichol. All rights reserved.
+//
+
+import Cocoa
+
+class AppDelegate: NSObject, NSApplicationDelegate {
+                            
+  @IBOutlet var window: NSWindow
+
+
+  func applicationDidFinishLaunching(aNotification: NSNotification?) {
+    // Insert code here to initialize your application
+  }
+
+  func applicationWillTerminate(aNotification: NSNotification?) {
+    // Insert code here to tear down your application
+  }
+
+
+}
+
---
+++ AVLTree/main.swift
@@ -0,0 +1,11 @@
+//
+//  main.swift
+//  AVLTree
+//
+//  Created by Alex Nichol on 6/10/14.
+//  Copyright (c) 2014 Alex Nichol. All rights reserved.
+//
+
+import Cocoa
+
+NSApplicationMain(C_ARGC, C_ARGV)
